738bef9f3f7149d12005dd6f3795c01f815b8470
Tisha Agarwal <Put your email here>
2024-11-14 11:40:33 +0530

    React changes

--- index.js
+++ index.js
@@ -1,94 +1,28 @@
-// console.log("hello");
-// var a=12;
-// console.log(a);
-// if(a>10){
-//     var b=23;
-//     console.log("a inside condition:"+a);
+function bulb_on(bulb) {
+    bulb.src = 'yeloe.png'; 
+}
+function bulb_off(bulb) {
 
-// }
-// console.log("a outside condition:"+b);
-// console.log("hello");
-// var a=12;
-// console.log(a);
-// if(a>10){
-//     var b=23;
-//     console.log("a inside condition:"+a);
+    bulb.src = 'lightt.png';
+}
 
-// }
-// console.log("b outside condition:"+b);
-
-// let res = "hi my age is ${a} ans your is ${b}";
-// console.log(res);
-// var a = 12
-// console.log(a);
-// if(a>10)
-// {
-//     let b =23
-//     console.log("a inside condition:"+a);
-// }
-
-// let a=12;
-// let b="12";
-// console.log(typeof b);
-// let today =Date();
-// console.log(today)
-
-// if(a===b)
-// {
-//     console.log("welcome")
-
-// }
-// else{
-//     console.log("Hello")
-// }
-// let myname = "Rahul"
-// let college = "ABES EC"
-// let result = hi,my name is ${myname} and i am doing engg from ${college}
-// console.log(result)
-
-// let key = "college";
-// const students ={
-    
-//     snmae  : "Aman",
-//     course : "Btech",
-//     college : "ABES EC"
-// }
-// console.log(students)
-// console.log("course:"+students.course);
-// const{college}=students;
-// console.log(college)
-
-// students[key]='akg'
-// console.log(students)
-
-// const students =[{
-    
-//     snmae  : "Aman",
-//     course : "Btech",
-//     college : "ABES EC"
-// },
-// {
-//     sname :"Amit",
-//     course :"mca",
-//     college : "akg"
-
-// }]
-
-// console.log(students[1]);
-
-//function
-// function greeting(msg="welcome")
-// {
-//     console.log("good morning" + msg)
-// }
-// greeting("greetings");
-
-// function sum(a,b)
-// {
-//     return a+b;
-// }
-// console.log(sum(12,20))
-console.log("hello world");
-let parent=document.getElementsByClassName("parent");
-console.log(parent);
-parent[0].innerHTML="<h2 style=color:red>DATA HAS CHANGED</h2>";
+document.getElementById('startButton').addEventListener('click', function() {
+    const num = parseInt(document.getElementById('numBulbs').value); 
+    const container = document.getElementById('bulbContainer');
+    container.innerHTML = ''; 
+    for (let i = 0; i < num; i++) {
+        const bulb = document.createElement('img');
+        bulb.className = 'bulb';
+        bulb.src = 'lightt.png'; 
+        bulb.style.height="40px"
+        bulb.style.width="40px"
+        bulb.addEventListener('click', function() {
+            if (bulb.src.includes('lightt.png')) {
+                bulb_on(bulb); 
+            } else {
+                bulb_off(bulb); 
+            }
+        });
+        container.appendChild(bulb);
+    }
+});
