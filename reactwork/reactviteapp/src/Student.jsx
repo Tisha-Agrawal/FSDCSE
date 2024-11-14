e3ecdd6195429dc2e26ad6e649e5516a0bcda9c5
Tisha Agarwal <Put your email here>
2024-11-14 13:13:23 +0530

    react work

--- reactwork/reactviteapp/src/Student.jsx
+++ reactwork/reactviteapp/src/Student.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './student.css'
 
-function Student({college,pic,name,branch,roll,section}) {
+function Student({data}) {
   return (
     <div className='icard'>
-        <table>
+       {/* {JSON.stringify(data)} */}
+       <table>
             <tbody>
-                <tr><td colSpan={2}>{college}</td></tr>
-                <tr><td colSpan={2}>{pic}</td></tr>
-                <tr><td>Name</td><td>{name}</td></tr>
-                <tr><td>Branch:</td><td>{branch}</td></tr>
-                <tr><td>Roll Number:</td><td>{roll}</td></tr>
-                <tr><td>Section:</td><td>{section}</td></tr>
+                <tr><td colSpan={2}>{data.college}</td></tr>
+                <tr><td colSpan={2}>{data.pic}</td></tr>
+                <tr><td>Name</td><td>{data.name}</td></tr>
+                <tr><td>Branch:</td><td>{data.branch}</td></tr>
+                <tr><td>Roll Number:</td><td>{data.roll}</td></tr>
+                <tr><td>Section:</td><td>{data.section}</td></tr>
                 
             </tbody>
         </table>
---
+++ reactwork/reactviteapp/src/UseStudentState.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+function UseStudentState(){
+    const[count,setCount]=useState(20);
+    return(
+        <div style={{color:"red"}}>{count}</div>
+
+    )
+}
+export default UseStudentState
